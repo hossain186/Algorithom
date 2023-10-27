68a6d15538c94877cd8c8f651bd8b15de333b2c8
hossain186 <hossainahmedsiam9@gmail.com>
2023-10-27 15:46:27 +0600

    commited

---
+++ sort/simple sort/simple sort O(n).py
@@ -0,0 +1,25 @@
+def findSmallest(arr):
+
+    smallest = arr[0] #just initial that,, suppose arr[0] is the smallest value
+    smallnumber_index = 0 #initial index value
+
+    #now chack array value accroding smallest value
+
+    for i in range(len(arr)):
+        if smallest > arr[i]:
+            smallest = arr[i]
+            smallnumber_index = i
+
+    return smallest
+
+
+new = []
+
+a = [2,1,4,2,6,4]
+for i in range(len(a)):
+
+    small = findSmallest(a)
+    new.append(small)
+    a.remove(small)
+
+print(new)
